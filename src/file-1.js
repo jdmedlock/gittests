39c7ab95e5dec08de2f1df46bf3295dd512c580d
jdmedlock <jdmedlock@gmail.com>
2020-09-06 07:27:52 -0500

    Inserted new line to file-1

--- src/file-1.js
+++ src/file-1.js
@@ -1,4 +1,5 @@
 // This is the first file created for the scenario 'Create Merge Conflict'
+// This line was inserteed while on branch-b
 const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 console.log (`file-1.js: List numbers in an array`)

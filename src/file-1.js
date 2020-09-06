622360cfbb95a5d1d086220d298d242c80f13645
jdmedlock <jdmedlock@gmail.com>
2020-09-06 07:22:02 -0500

    file-1.js before merge conflict

---
+++ src/file-1.js
@@ -0,0 +1,6 @@
+// This is the first file created for the scenario 'Create Merge Conflict'
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log (`file-1.js: List numbers in an array`)
+list.forEach(i => console.log(`...${i}`))
+console.log(`...Finished!`)

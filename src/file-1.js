6edf5208af60e25a051fe7e7c72f528ec4ebf3ac
jdmedlock <jdmedlock@gmail.com>
2021-01-13 18:09:10 -0600

    Remove superflouous console.log

--- src/file-1.js
+++ src/file-1.js
@@ -5,4 +5,3 @@ const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log (`file-1.js: List numbers in an array`);
 list.forEach(i => console.log(`...${i}`));
 console.log(`...Finished!`);
-console.log(`But is SEAN finished???`);
